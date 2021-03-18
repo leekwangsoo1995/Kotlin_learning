839d27191c70065b834a859bcc167b1096e9aad9
leekwangsoo1995 <jayzisgodminion@gmail.com>
2021-03-18 15:53:00 +0900

    Initial commit

---
+++ src/app.kt
@@ -0,0 +1,11 @@
+
+package ssample
+
+fun  main(){
+    println("hello world");
+    val half = Rational(1,2)
+    println(half.denominator);
+}
+
+class Rational(val numerator:Int , val denominator:Int)
+
